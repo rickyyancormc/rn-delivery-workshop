f1878a48e5191891af142ad9f61d4277354b489d
Ricky Yancor <rickyyancor@Ricky-MangoChangos-MacBook-Pro.local>
2025-08-27 02:58:34 -0600

    changing script

--- scripts/start_emulator_2.sh
+++ scripts/start_emulator_2.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Criando AVD para Emulator 2..."
-avdmanager create avd --force -n Pixel_8_Pro_API_35 --abi 'google_apis/x86_64' --package 'system-images;android-28;google_apis;x86_64' --device 'Nexus'
+avdmanager create avd --force -n Pixel_8_Pro_API_35 --abi 'google_apis/x86_64' --package 'system-images;android-33;default;x86_64' --device 'Nexus'
 
 echo "Iniciando Emulator 2 na porta 5556..."
 nohup /usr/local/lib/android/sdk/emulator/emulator -port 5556 -avd Pixel_8_Pro_API_35 -no-window -gpu swiftshader_indirect -no-snapshot -noaudio -no-boot-anim &
